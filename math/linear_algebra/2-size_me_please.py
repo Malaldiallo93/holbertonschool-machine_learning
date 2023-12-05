dabb911741b70148b79c5d62a63b73213b0585fa
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-12-05 14:51:46 +0100

    update matrix_shape function

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+""" matrix_shape function """
+
+
 def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list.
+
+    Args:
+        matrix (n-dimensional array): The matrix to find the shape of.
+
+    Returns:
+        list: The dimensions of the array as a list.
+    """
     shape = []
     while type(matrix) == list:
         shape.append(len(matrix))
