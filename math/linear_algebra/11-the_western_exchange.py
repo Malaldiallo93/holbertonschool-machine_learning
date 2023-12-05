ddb1a336057b81518949dc348b41d8b2126b0d4d
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-12-05 22:47:34 +0100

    a function def np_transpose(matrix): that transposes matrix.

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""np_transpose function"""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray.
+
+    Args:
+    - matrix (numpy.ndarray): The input numpy array.
+
+    Returns:
+    - numpy.ndarray: The transposed array.
+    """
+    return matrix.T
