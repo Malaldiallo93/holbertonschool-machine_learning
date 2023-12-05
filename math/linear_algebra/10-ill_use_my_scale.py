63e04e958ddb19ea36dbc3784efe7739289ddc66
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-12-05 22:44:20 +0100

    a function def np_shape(matrix): that calculates the shape of a numpy.ndarray.

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""np_shape function"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray.
+
+    Args:
+    - matrix (numpy.ndarray): The input numpy array.
+
+    Returns:
+    - tuple of ints: The shape of the array.
+    """
+    return matrix.shape
