4adef041e82898c5f0a46c0a47064a83459b3413
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-12-16 18:46:33 +0100

    Update the class Poisson.

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,58 +1,92 @@
 #!/usr/bin/env python3
 """poisson_function"""
 
-import math
-
 
 class Poisson:
     """
-    Class representing a Poisson distribution.
+    Represents a Poisson distribution.
+
+    Attributes:
+        lambtha (float): The expected number of occurrences
+        in a given time frame.
+
+    Raises:
+        TypeError: If `data` is not a list.
+        ValueError: If `data` does not contain at least two values,
+        or if `lambtha` is not a positive value.
     """
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Constructor of the Poisson class.
+        Initializes a Poisson distribution.
 
         Args:
-            data (list, optional): List of data to use to
-            estimate the distribution.
-            lambtha (float, optional): Expected number of occurrences
-            in a given time interval.
+            data (list, optional): A list of data points to be used
+            to estimate the distribution.
+            lambtha (float, optional): The expected number of
+            occurrences in a given time frame.
 
         Raises:
-            ValueError: If lambtha is not a positive value.
-            TypeError: If data is not a list.
-            ValueError: If data does not contain at least two values.
+            TypeError: If `data` is not a list.
+            ValueError: If `data` does not contain at least two values,
+            or if `lambtha` is not a positive value.
         """
+
+        # Check if data is None or a list
+        if data is not None and not isinstance(data, list):
+            raise TypeError('data must be a list')
+
+        # Check if data contains at least 2 values
+        if data is not None and len(data) < 2:
+            raise ValueError('data must contain multiple values')
+
+        # Check if lambtha is a positive value
+        if lambtha <= 0:
+            raise ValueError('lambtha must be a positive value')
+
+        # Set lambtha based on data or parameter
         if data is None:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            else:
-                self.lambtha = float(lambtha)
+            self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            elif len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            else:
-                self.lambtha = float(sum(data) / len(data))
+            # Calculate lambtha of data
+            self.lambtha = sum(data) / len(data)
 
     def pmf(self, k):
         """
-        Calculates the value of the PMF for a given number of "successes".
+        Calculates the value of the PMF for a given number of "successes."
 
         Args:
-            k (int): The number of "successes".
+            k (int): The number of "successes."
 
         Returns:
             float: The PMF value for k.
         """
+        # Convert k to an integer
         k = int(k)
+
+        # Check if k is out of range
         if k < 0:
             return 0
-        else:
-            return (
-                (self.lambtha ** k) *
-                (math.exp(-self.lambtha)) /
-                math.factorial(k)
-                )
+
+        # Calculate the PMF value
+        pmf_value = (
+            (self.lambtha ** k) *
+            (2.71828 ** (-self.lambtha)) /
+            self.factorial(k)
+        )
+        return pmf_value
+
+    @staticmethod
+    def factorial(n):
+        """
+        Calculates the factorial of a number.
+
+        Args:
+            n (int): The number.
+
+        Returns:
+            int: The factorial of n.
+        """
+        if n == 0:
+            return 1
+        return n * Poisson.factorial(n - 1)
