c6aad8f264c38c1824e26adf8fc1849c652c6bd5
Malaldiallo93 <mohamedmalal93@gmail.com>
2024-01-23 14:30:59 +0100

    update documentation

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,69 +1,38 @@
 #!/usr/bin/env python3
-"""class NeuralNetwork"""
+"""
+    Class NeuralNetwork
+"""
+
 import numpy as np
 
 
 class NeuralNetwork:
+    """
+        class NeuralNetwork
+    """
+
     def __init__(self, nx, nodes):
         """
-        Constructor method for NeuralNetwork instances.
+            class constructor
 
-        Args:
-            nx (int): The number of input features.
-            nodes (int): The number of nodes in the hidden layer.
-
-        Raises:
-            TypeError: If nx or nodes is not an integer.
-            ValueError: If nx or nodes is less than 1.
+            :param nx: number of input features
+            :param nodes: number of nodes in the hidden layer
         """
+
         if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
-        elif nx < 1:
-            raise ValueError('nx must be a positive integer')
-
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
         if not isinstance(nodes, int):
-            raise TypeError('nodes must be an integer')
-        elif nodes < 1:
-            raise ValueError('nodes must be a positive integer')
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
 
-        self.__W1 = np.random.normal(0, 1, (nodes, nx))
-        self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = 0
-
-        self.__W2 = np.random.normal(0, 1, (1, nodes))
-        self.__b2 = 0
-        self.__A2 = 0
-
-    @property
-    def W1(self):
-        return self.__W1
-
-    @property
-    def b1(self):
-        return self.__b1
-
-    @property
-    def A1(self):
-        return self.__A1
-
-    @property
-    def W2(self):
-        return self.__W2
-
-    @property
-    def b2(self):
-        return self.__b2
-
-    @property
-    def A2(self):
-        return self.__A2
-
-    @A1.setter
-    def A1(self, value):
-        """
-        Setter method for A1 attribute.
-
-        Args:
-            value: The value to set A1 to.
-        """
-        self.__A1 = value
+        # Initialize public instance attribute
+        # W1 & W2 normal distribution
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
