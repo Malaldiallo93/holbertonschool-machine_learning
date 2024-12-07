2c46a41b75245e6ca21436ac8c01c9841fe12fd9
Malaldiallo93 <mohamedmalal93@gmail.com>
2024-12-07 21:26:14 +0100

    update import and pycodestyle

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -2,19 +2,17 @@
 """
     Class Dataset to load and preprocess the dataset
 """
-import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
-
 
 class Dataset:
     """
-        Class to loads and prepas a dataset for machine translation
+        Class to load and prepare a dataset for machine translation
     """
     def __init__(self):
         """
-            class init
+            Class initializer
         """
-        self.data_train, self.data_valid =\
+        self.data_train, self.data_valid = \
             tfds.load('ted_hrlr_translate/pt_to_en',
                       split=['train', 'validation'],
                       as_supervised=True)
@@ -22,22 +20,22 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """
-            creates sub-word tokenizers for our dataset
-
-        :param data: tf.data.Dataset, tuple (pt,en)
-            pt: tf.Tensor portuguese sentence
-            en: tf.Tensor english sentence
+            Creates sub-word tokenizers for our dataset
+        :param data: tf.data.Dataset, tuple (pt, en)
+            pt: tf.Tensor, Portuguese sentence
+            en: tf.Tensor, English sentence
             max vocab size : 2**15
-
         :return: tokenizer_pt, tokenizer_en
-            respectively portuguese and english tokenizer
+            Respectively Portuguese and English tokenizers
         """
+        # Convert tf.Tensors to strings for tokenizer
+        pt_sentences = (pt.numpy().decode('utf-8') for pt, _ in data.as_numpy_iterator())
+        en_sentences = (en.numpy().decode('utf-8') for _, en in data.as_numpy_iterator())
+
         self.tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, _ in data),
-            target_vocab_size=2**15
+            pt_sentences, target_vocab_size=2**15
         )
         self.tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for _, en in data),
-            target_vocab_size=2 ** 15
+            en_sentences, target_vocab_size=2**15
         )
         return self.tokenizer_pt, self.tokenizer_en
