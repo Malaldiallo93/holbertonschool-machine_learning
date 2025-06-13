034dab8a0de50b93ed3f33b257569b29febbc737
Malaldiallo93 <mohamedmalal93@gmail.com>
2025-06-13 17:33:46 +0200

    Write a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,13 +1,16 @@
-DELIMITER $$
+-- creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+DROP FUNCTION IF EXISTS SafeDiv //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
+    ELSE
+        RETURN a / b;
     END IF;
-    RETURN a DIV b;
-END$$
+END //
 
 DELIMITER ;
